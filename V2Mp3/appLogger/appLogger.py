49fb7b2e62f05b3e05dad03aee36ace01053bb97
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-07-28 20:22:28 -0500

    Added docstring to `setLogger` method
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- V2Mp3/appLogger/appLogger.py
+++ V2Mp3/appLogger/appLogger.py
@@ -2,13 +2,24 @@ import logging
 
 
 def setLogger(name) -> logging.Logger:
+    """
+    Creates and returns program logger object.
+
+    ---
+
+    :param name: The name of the logger
+    :type name: :class:`str`
+    :return: A logger object
+    :rtype: :class:`logging.Logger`
+    """
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     logFormatter = logging.Formatter(
         '[%(asctime)s - %(levelname)s] : %(message)s\n', "%Y-%m-%d %H:%M:%S")
 
-    logHandler = logging.FileHandler(r'.\logs\v2mp3Log.log', 'a')
+    logHandler = logging.FileHandler(r'.\logs\v2mp3Log.log')
     logHandler.setFormatter(logFormatter)
 
     logger.addHandler(logHandler)
